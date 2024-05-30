660aba886429b0a3241c06d08ebbfc52c0e9649d
naveedalirehmani <naveedalirehmani321@gmail.com>
2024-05-30 19:54:31 +0500

    added supporting components.

--- portal/src/components/SideNavbar.tsx
+++ portal/src/components/SideNavbar.tsx
@@ -1,57 +1,38 @@
-/** @format */
 "use client";
 
 import { Navbar } from "./ui/nav";
 
 type Props = { children: React.ReactNode };
 
-import { ShoppingCart, PlusCircle, List, DollarSign } from "lucide-react";
+import { MailIcon, Send, Trash, Archive } from "lucide-react";
 import { routes } from "@/types/routes";
 
 export default function SideNavbar({ children }: Props) {
   return (
     <div>
       <Navbar
-      links={[
-        // {
-        //   title: "Dashboard",
-        //   href: routes.dashboard,
-        //   icon: LayoutDashboard,
-        //   variant: "default",
-        // },
-        {
-          title: "Add Product",
-          href: routes.add_product,
-          icon: <PlusCircle className="h-5 w-5" />,
-        },
-        {
-          title: "All Products",
-          href: routes.all_products,
-          icon: <List className="h-5 w-5" />,
-        },
-        // {
-        //   title: "Users",
-        //   href: routes.users,
-        //   icon: UsersRound,
-        //   variant: "ghost",
-        // },
-        {
-          title: "Orders",
-          href: routes.orders,
-          icon: <ShoppingCart className="h-5 w-5" />,
-        },
-        {
-          title: "Transactions",
-          href: routes.transactions,
-          icon: <DollarSign className="h-5 w-5" />,
-        },
-        // {
-        //   title: "Settings",
-        //   href: routes.settings,
-        //   icon: Settings,
-        //   variant: "ghost",
-        // },
-      ]}
+        links={[
+          {
+            title: "Primary",
+            href: routes.primary,
+            icon: <MailIcon className="h-5 w-5" />,
+          },
+          {
+            title: "Sent",
+            href: routes.sent,
+            icon: <Send className="h-5 w-5" />,
+          },
+          {
+            title: "Trash",
+            href: routes.trash,
+            icon: <Trash className="h-5 w-5" />,
+          },
+          {
+            title: "Junk",
+            href: routes.junk,
+            icon: <Archive className="h-5 w-5" />,
+          },
+        ]}
       >
         {children}
       </Navbar>
